5f79181a8c614a9a4492ecf29fa744bdd01b6d6d
siykt <cnsiykt@163.com>
2023-05-25 08:43:30 +0800

    feat: router demo

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,14 @@
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+
 const App = () => {
-  return <div>Home</div>;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </HashRouter>
+  );
 };
 
 export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import * as SC from './styles';
+
+interface HomeProps {}
+
+const Home: FC<HomeProps> = () => {
+  return <SC.HomeContainer>Home</SC.HomeContainer>;
+};
+
+export default Home;
