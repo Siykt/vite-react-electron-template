cf1e7681aeb1958ff8d8c6329d78b1d90320e4f9
siykt <cnsiykt@163.com>
2023-05-25 09:02:56 +0800

    chore: e2e

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 原项目:
 
 [electron-vite-react](https://github.com/electron-vite/electron-vite-react)
+
+## e2e 测试
+
+e2e 测试需要先编译项目
+
+```bash
+pnpm pree2e
+```
+
+再执行测试
+
+```bash
+pnpm e2e
+```
--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,8 +1,9 @@
-import { test, expect, _electron as electron } from "@playwright/test";
+import { test, expect, _electron as electron } from '@playwright/test';
 
-test("homepage has title and links to intro page", async () => {
-  const app = await electron.launch({ args: [".", "--no-sandbox"] });
+test('homepage has title and links to intro page', async () => {
+  const app = await electron.launch({ args: ['.', '--no-sandbox'] });
   const page = await app.firstWindow();
-  expect(await page.title()).toBe("Electron + Vite + React");
-  await page.screenshot({ path: "e2e/screenshots/example.png" });
+  expect(await page.title()).toBe('Vite React Electron Template');
+  expect(page.getByText('Home')).toBeTruthy();
+  await page.screenshot({ path: 'e2e/screenshots/example.png' });
 });
